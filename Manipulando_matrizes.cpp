600c99bf827e983516a397947a27755027b5075d
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-17 14:19:53 +0000

    d

---
+++ Manipulando_matrizes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main(){
+
+int linha, coluna;
+
+int matriz [2] [2];
+
+for(linha=0, coluna <2; linha++) {
+
+ for(coluna=0, linha <2; coluna++) {
+
+  printf("\n Digite os valores da matriz para a linha %d, e coluna %d", linha +1, coluna +1);
+  scanf("%d" &matriz [linha][coluna]);
+ }
+
+}
+
+for(linha=0; linha < 2; linha++){
+
+    for(coluna=0; coluna<2; linha++){
+     
+     printf("%d \t\n", matriz[linha][coluna]);
+      printf("\n Veja a sua Matriz: \n")
+      printf("\n\n");
+
+    }
+
+
+}
+
+
+return 0;
+}
--- Primeira_letra_maiuscula.cpp
+++ Primeira_letra_maiuscula.cpp
@@ -11,14 +11,14 @@ printf("\n Digite o sobrenome do aluno ou aluna: \n");
 
 scanf("%s", &nome);
 
-for(i=0; nome[i]!=' '; i++)
+for(i=0; nome[i]!=' '; i++) //enumera todos os caracteres do vetor de 0 a 30
 {
 
-nome[i] = toupper (nome[i]);
+nome[i] = toupper (nome[i]); //coloca o valor dentro do vetor em letra maiuscula, começando do 0 ao 30
 
 printf("\n Sobrenome convertido: %s \n \n",nome );
 
-break;
+break; //corta o for na primeira execução deixando somente a primeira letra maiuscula
 }
 
 
