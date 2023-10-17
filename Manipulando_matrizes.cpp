24fcb7bd63187d89c4a9f201adc8d465368afc93
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-17 14:35:08 +0000

    w

--- Manipulando_matrizes.cpp
+++ Manipulando_matrizes.cpp
@@ -6,26 +6,26 @@ int linha, coluna;
 
 int matriz [2] [2];
 
-for(linha=0; coluna <2; linha++) {
+for(linha=0; coluna <=2; linha++) {
 
- for(coluna=0; linha <2; coluna++) {
+ for(coluna=0; linha <=2; coluna++) {
 
   printf("\n Digite os valores da matriz para a linha %d, e coluna %d", linha +1, coluna +1);
   scanf("%d", &matriz [linha][coluna]);
+  
  }
 
 }
 
-for(linha=0; linha < 2; linha++){
+for(linha=0; linha <=2; linha++){
 
-    for(coluna=0; coluna<2; linha++){
+    for(coluna=0; coluna<=2; linha++){
      
-     printf("%d \t\n", matriz[linha][coluna]);
+      printf("%d \t\n", matriz[linha][coluna]);
       printf("\n Veja a sua Matriz: \n");
       printf("\n\n");
-
+      
     }
-
 
 }
 
