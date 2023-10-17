aa687736fb50b6d8dd4cf8ecb9332fedfeb11f46
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-17 17:01:42 +0000

    u

--- Manipulando_matrizes.cpp
+++ Manipulando_matrizes.cpp
@@ -6,10 +6,10 @@ int main() {
     int linhas, colunas;
 
     printf("Digite o número de linhas da matriz: ");
-    scanf("%d", &linhas);
+    scanf("%d", &linhas); //formando a matriz captura do numero de linhas
 
     printf("Digite o número de colunas da matriz: ");
-    scanf("%d", &colunas);
+    scanf("%d", &colunas); //formando a matriz captura do numero de colunas
 
     int matriz[linhas][colunas];
 
@@ -17,8 +17,8 @@ int main() {
 
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
-            printf("\n Digite o elemento da linha %d e coluna %d: ", i + 1, j + 1);
-            scanf("%d", &matriz[i][j]);
+            printf("\n Digite o elemento da linha %d e coluna %d: ", i + 1, j + 1); //capturar os elementos enumerando em linhas e colunas
+            scanf("%d", &matriz[i][j]); //captura da matriz
         }
     }
 
