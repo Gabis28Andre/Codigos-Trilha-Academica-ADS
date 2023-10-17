4d9ead7a5ac1200754eb29d9312e42af01edff2d
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-17 19:09:18 +0000

    d

--- Calculo_massa_reacao_protecao.cpp
+++ Calculo_massa_reacao_protecao.cpp
@@ -1,4 +1,9 @@
 
+
+/*Situação Problema*/
+
+
+
 #include <iostream>
 
 
@@ -8,9 +13,9 @@ const float mA=321.43;
 const float mB= 150.72; //valores passados na questão
 
 //calcular com pesos passados na questão
-printf("\n 1,2 : 1,0 \t = %f", (1,2*mA+1*mB));
-printf("\n 1,4 : 1,0 \t = %f", (1,4*mA+1*mB));
-printf("\n 1,0 : 1,6 \t = %f", (1*mA+1,6*mB));
+printf("\n 1,2 : 1,0 \t = %.3f", (1,2*mA+1*mB));
+printf("\n 1,4 : 1,0 \t = %.3f", (1,4*mA+1*mB));
+printf("\n 1,0 : 1,6 \t = %.3f \n", (1*mA+1,6*mB));
 
 
 return (a* mA) + (b* mB);
@@ -21,15 +26,17 @@ int main(){
 
 float a=0, b=0, resultado=0;
 
-printf("\n Digite a massa do elementp A");
+printf("\n Digite a massa do elemento A: ");
 scanf("%f", &a);
 
-printf("\n Digite a massa do elemento B");
+printf("\n Digite a massa do elemento B: ");
 scanf("%f", &b);
 
 resultado = calcularMassa(a, b); //chamando a função
 
-printf("\n\t Resultado: %f", resultado);
+printf("\n\t Resultado: %.2f", resultado);
+printf("\n\n");
+printf("\n\n");
 
 }
 
