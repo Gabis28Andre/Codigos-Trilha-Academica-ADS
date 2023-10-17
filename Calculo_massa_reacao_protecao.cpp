93e5e5ba417393aaefdb6ff74a240618cca17de2
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-17 19:32:03 +0000

    6

--- Calculo_massa_reacao_protecao.cpp
+++ Calculo_massa_reacao_protecao.cpp
@@ -4,7 +4,7 @@
 Foi solicitado a voce para automatizar um calculo de uma reação chamada de proteção.
 Nessa reação um composto A de massa = 321,43 g/mol será comado a um composto B de 
 massa = 150,72 g/mol. Seu programa além de calcular o composto com base nas informações
-do usuário, deverá também exibir os valores de referencia das combinações (pesos): 
+do usuário, deverá também exibir uma tabela com os valores de referencia das combinações (pesos): 
 (1,2 : 1,0), (1,4 : 1,0), (1,0 : 1,6).
 
 //interpretando : Quer uma função que calcule (some) a massa de A e de B multiplicado por esses pesos
@@ -22,9 +22,9 @@ const float mA=321.43;
 const float mB= 150.72; //valores passados na questão
 
 //calcular com pesos passados na questão
-printf("\n 1,2 : 1,0 \t = %.3f", (1,2*mA+1*mB));
-printf("\n 1,4 : 1,0 \t = %.3f", (1,4*mA+1*mB));
-printf("\n 1,0 : 1,6 \t = %.3f \n", (1*mA+1,6*mB));
+printf("\n 1,2 : 1,0 \t = %.3f", ((1,2*mA)+(1*mB)));
+printf("\n 1,4 : 1,0 \t = %.3f", ((1,4*mA)+(1*mB)));
+printf("\n 1,0 : 1,6 \t = %.3f \n", ((1*mA)+(1,6*mB)));
 
 
 return (a* mA) + (b* mB);
