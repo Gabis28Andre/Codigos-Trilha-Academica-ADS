78bdde6f2564916d8119163ef6998efb975b08e2
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-18 17:38:29 +0000

    r

---
+++ Funcao_recursiva_raizes_newton.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<math.h>
+
+float calcularraiz(float n, float raizant){
+
+float raiz = (pow(raizant, 2)+ n) / (2 * raizant);
+
+if (fabs(raiz - raizant)<0.001)
+return raiz;
+return calcularraiz(n, raiz);
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+    float numero, raiz;
+
+    printf("\n Digite um numero para calcular a raiz: \n");
+    scanf("%f", &numero);
+    raiz = calcularraiz(numero, numero/2);
+    
+    printf("\n Raiz quadrada função: = %f\n", raiz);
+
+    return 0;
+}
