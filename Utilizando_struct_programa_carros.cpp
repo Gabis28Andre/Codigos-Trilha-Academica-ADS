22cc6375332616403dada65ea2ed0aa3a8a6ddba
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-19 18:40:30 +0000

    3

---
+++ Utilizando_struct_programa_carros.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+
+
+struct Carro
+{
+    char modelo [20];
+    int ano;
+    int pot; 
+};
+
+int escolha(int a[], int b[]){
+
+for (int i = 0; i < 3; i++)
+{
+   
+   printf("\n Digite 1......Civic: \n");
+   printf("\n Digite 2......Golf: \n");
+   printf("\n Digite 1......Corolla: \n");
+   scanf("\t %d", &a[i]);
+
+}
+
+
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+    Carro car1;
+    Carro car2;
+    Carro car3;
+
+
+    car1.modelo = "Civic";
+    car1.pot;
+    car1.cor;
+
+
+
+
+    return 0;
+}
