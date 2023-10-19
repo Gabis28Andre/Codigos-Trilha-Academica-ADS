7d35a05940d5d10b0aee50926f73a67cf5cfa141
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-19 19:39:06 +0000

    3

--- Utilizando_struct_programa_carros.cpp
+++ Utilizando_struct_programa_carros.cpp
@@ -9,59 +9,89 @@ struct Carro
     string cor;
     int ano;
     int pot; 
+    int velMax;
+    int vel;
+
+  void insere(string stmodelo, string stcor, int stano, int stpot, int stvelMax, int stvel) {
+    modelo = stmodelo;
+    cor = stcor;
+    ano = stano;
+    pot = stpot;
+    velMax = stvelMax;
+    vel = 0;
+  }
+
+  void mostra(){
+   
+
+   cout << "\n Nome do modelo: " << modelo << "\n";
+   cout << "Cor: " << cor << "\n";
+   cout << "Ano: " << ano << "\n";
+   cout << "Potencia: " << pot << "\n";
+   cout << "VelocidadeMax: " << velMax << "\n";
+   cout << "Velocidade Atual: " << vel << "\n";
+
+  }
+
+  
+  void movvel(int movimento){
+    
+  vel = movimento;
+
+  if(vel > velMax) {
+    vel = velMax;
+
+  }
+
+  if(vel < 0){
+    vel = 0;
+  }
+
+ }
+
+
+
+
 };
 
+void escolha(){
+
+
+   printf("\n Digite 1......Civic: \n");
+   printf("\n Digite 2......Golf: \n");
+   printf("\n Digite 3......Corolla: ");
+   
+
+  
+
+}
 
 int main(int argc, char const *argv[])
 {
 
-    Carro car1;
+    Carro car1, car2, car3;
+    //modelo, cor, ano, potencia, velmax, vel
+    car1.insere ("Civic", "Prata", 2013, 500, 213, 0);
+    car2.insere ("Golf", "Branco", 2010, 300, 213, 40);
+    car3.insere ("Corolla", "Cinza", 2018, 280, 213, 40);
+    
 
-    car1.modelo = "Civic";
-    car1.pot = 350;
-    car1.cor = "Prata";
-    car1.ano = 2013;
+    escolha();
 
-    Carro car2;
-     
-    car2.modelo = "Golf";
-    car2.pot = 250;
-    car2.cor = "Branco";
-    car2.ano = 2010;
+     int a;
+    scanf("\t %d", &a);
 
-
-    Carro car3;
-
-    car3.modelo = "Corolla";
-    car3.pot = 280;
-    car3.cor = "Cinza";
-    car3.ano = 2018;
-
-   int a;
-   printf("\n Digite 1......Civic: \n");
-   printf("\n Digite 2......Golf: \n");
-   printf("\n Digite 3......Corolla: ");
-   scanf("\t %d", &a);
-
-   if(a == 1){
-   cout << "\n Nome do modelo: " << car1.modelo << "\n";
-   cout << "Cor: " << car1.cor << "\n";
-   cout << "Ano: " << car1.ano << "\n";
-   cout << "Potencia: " << car1.pot << "\n";
+    if(a == 1){
+    car1.mostra();
    }
+   
 
     if(a == 2){
-   cout << "\n Nome do modelo: " << car2.modelo << "\n";
-   cout << "Cor: " << car2.cor << "\n";
-   cout << "Ano: " << car2.ano << "\n";
-   cout << "Potencia: " << car2.pot << "\n";
+   car2.mostra();
    }
 
     if(a == 3){
-   cout << "\n Nome do modelo: " << car3.modelo << "\n";
-   cout << "Cor: " << car3.cor << "\n";
-   cout << "Ano: " << car3.ano << "\n";
-   cout << "Potencia: " << car3.pot << "\n";
+   car3.mostra();
    }
 
 printf("\n\n");
