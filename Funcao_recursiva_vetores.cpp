8ef012f49dcf403ee7b05ad0ffc71099966763ed
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-19 13:01:09 +0000

    h

--- Funcao_recursiva_vetores.cpp
+++ Funcao_recursiva_vetores.cpp
@@ -1,17 +1,8 @@
+//programa para inserir valor duplicado com função recursiva
+
 #include <iostream>
 
-void inserir(int a[]){//utilizando vetor como parametro e não retorna vetor
 
-int i;
-
-for(i=0; i<3; i++ ) 
- {
- printf("\n Digite o valor %d\n", i);
- scanf("%d", &a[i]); //
-
-}
-
-}
 
 int main(int argc, char const *argv[])
 {
@@ -19,8 +10,36 @@ int main(int argc, char const *argv[])
 
     printf("\n\t Preencha o Vetor: \n");
 
-
     inserir(numero);
 
+    printf("\n valor final: ");
+
+    imprimir(numero);
+
     return 0;
+}
+
+
+void inserir(int a[]){//utilizando vetor como parametro e não retorna vetor
+
+int i;
+
+for(i=0; i<3; i++ ) 
+ {
+ printf("\n Digite o valor do indice: %d\n\t", i);
+ scanf("%d", &a[i]); //
+
+}
+
+}
+
+void imprimir(int b []){ // só utiliza o vetor como parametro não retorna nenhum vetor
+
+int i;
+for(i=0; i<3; i++){
+
+    prinf("\n Numeros [%d], %d", i, (2*b[i]))
+
+}
+
 }
