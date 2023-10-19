68ea24d0e2d3a04ec5d300ee1ad1ac8936d21a00
Gabis28Andre <143129675+Gabis28Andre@users.noreply.github.com>
2023-10-19 13:09:06 +0000

    g

--- Funcao_recursiva_vetores.cpp
+++ Funcao_recursiva_vetores.cpp
@@ -2,32 +2,14 @@
 
 #include <iostream>
 
-
-
-int main(int argc, char const *argv[])
-{
-    int numero[3];
-
-    printf("\n\t Preencha o Vetor: \n");
-
-    inserir(numero);
-
-    printf("\n valor final: ");
-
-    imprimir(numero);
-
-    return 0;
-}
-
-
 void inserir(int a[]){//utilizando vetor como parametro e não retorna vetor
 
 int i;
 
 for(i=0; i<3; i++ ) 
  {
- printf("\n Digite o valor do indice: %d\n\t", i);
- scanf("%d", &a[i]); //
+ printf("\n Digite o valor do indice [%d]: \t", i);
+ scanf("%d", &a[i]); // função que escaneia o valor que vai ser recebido pelo indice na variavel i
 
 }
 
@@ -36,10 +18,28 @@ for(i=0; i<3; i++ )
 void imprimir(int b []){ // só utiliza o vetor como parametro não retorna nenhum vetor
 
 int i;
-for(i=0; i<3; i++){
+for(i=0; i<3; i++){ //função que apresenta os indices
 
-    prinf("\n Numeros [%d], %d", i, (2*b[i]))
-
-}
+    printf("\n Numeros [%d], %d", i, (2*b[i])); //função que dobra o valor que os indices recebem
 
 }
+ printf("\n\n"); //espaçamento só no final
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int numero[3];
+
+    printf("\n\t Preencha o Vetores para serem dobrados: \n");
+
+    inserir(numero);
+
+    printf("\n\t Valor dos Vetores: \n");
+
+    imprimir(numero);
+
+    return 0;
+}
+
+
